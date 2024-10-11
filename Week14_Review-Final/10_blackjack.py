428d72174b55477fac54048ff7f443196bb7c2ea
DucKyWay <80985006+DucKyWay@users.noreply.github.com>
2024-10-11 08:55:53 +0700

    finish blackjack

--- Week14_Review-Final/10_blackjack.py
+++ Week14_Review-Final/10_blackjack.py
@@ -1,27 +1,26 @@
 def show_high(hand):
-    if 'K' in hand:
-        return 'K'
-    elif 'Q' in hand:
-        return 'Q'
-    elif 'J' in hand:
-        return 'J'
+    if 'K' in hand:     return 'K'
+    elif 'Q' in hand:   return 'Q'
+    elif 'J' in hand:   return 'J'
     else:
         hand = [1 if i == 'A' else i for i in hand]
         hand = [int(i) for i in hand]
         return max(hand)
 
 hand = input().split(' ')
-hand_op = []
+hand_op, hand_use = [], []
 for i in range(len(hand)):
-    if hand[i] == 'A': hand_op.append(1)
-    elif hand[i] in 'JQK': hand_op.append(10)
-    else: hand_op.append(int(hand[i]))
+    if hand[i] == 'A':      hand_op.append(1)
+    elif hand[i] in 'JQK':  hand_op.append(10)
+    else:                   hand_op.append(int(hand[i]))
 score = 0
 for i in range(len(hand_op)):
     if score > 16:
         break
     else:
         score += hand_op[i]
-print(show_high(hand))
+        hand_use.append(hand[i])
+
+print(show_high(hand_use))
 if score <= 21: print(score)
 else: print('busted')
