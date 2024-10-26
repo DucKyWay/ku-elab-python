09a463de5a81853bba960997ef32e2bc899f706a
suphawinee.ch <suphawinee.ch@ku.th>
2024-10-26 15:10:57 +0000

    wip morsor.py

--- Week14_Dict.py/p_14-06_mor-sor.py
+++ Week14_Dict.py/p_14-06_mor-sor.py
@@ -1,3 +1,9 @@
+def count_on_class(on_class):
+    dt = {}
+    for i in range(len(on_class)):
+        dt[i] = sum(j for j in on_class[i]) 
+    return dt
+
 exercise = input().split(" ") # จำนวนข้อของแต่ละชุด
 exercise = [int(_) for _ in exercise]
 
@@ -6,16 +12,19 @@ standard = [float(_) for _ in standard]
 
 nisit = int(input())
 ns_on_class = [0 for i in range(nisit)]
-ns_passes = [0 for i in range(nisit)]
-
 for i in range(nisit):
     ns_on_class[i] = input().split(" ")
-for i in range(nisit):
+    ns_on_class[i] = [int(_) for _ in ns_on_class[i]] 
+
+ns_passes = [0 for i in range(nisit)]
+for i in range(nisit): 
     ns_passes[i] = input().split(" ")
+    ns_passes[i] = [int(_) for _ in ns_passes[i]] 
+
 
 print(exercise)
 print(standard)
 print("== การเข้าห้องเรียนของแต่ละคน ==")
-for i in ns_on_class: print(i)
+print(count_on_class(ns_on_class))
 print("== จำนวนข้อที่ทำผ่านของแต่ละ ex ==")
 for i in ns_passes: print(i)
