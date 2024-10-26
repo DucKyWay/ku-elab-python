1147fa341a4610965fad6cbdf1ce990f9af7321b
suphawinee.ch <suphawinee.ch@ku.th>
2024-10-26 09:38:53 +0000

    back to code

--- Week14_Dict.py/p_14-06_mor-sor.py
+++ Week14_Dict.py/p_14-06_mor-sor.py
@@ -1,14 +1,21 @@
-exercise = input().split(" ")
+exercise = input().split(" ") # จำนวนข้อของแต่ละชุด
 exercise = [int(_) for _ in exercise]
 
-passes = input().split(" ")
-passes = [float(_) for _ in passes]
+standard = input().split(" ")
+standard = [float(_) for _ in standard]
 
 nisit = int(input())
+ns_on_class = [0 for i in range(nisit)]
+ns_passes = [0 for i in range(nisit)]
+
 for i in range(nisit):
-    on_class = input().split(" ")
-    on_class = []
-    for i in range(1, len(exercise)):
-        pass
+    ns_on_class[i] = input().split(" ")
+for i in range(nisit):
+    ns_passes[i] = input().split(" ")
+
 print(exercise)
-print(passes)
+print(standard)
+print("== การเข้าห้องเรียนของแต่ละคน ==")
+for i in ns_on_class: print(i)
+print("== จำนวนข้อที่ทำผ่านของแต่ละ ex ==")
+for i in ns_passes: print(i)
