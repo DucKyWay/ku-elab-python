4abb1cc79ef800abfb53645839415b26a9638eb7
suphawinee.ch <suphawinee.ch@ku.th>
2024-10-27 03:47:25 +0000

    no idea abount one case

--- Week14_Dict.py/p_14-05_bid.py
+++ Week14_Dict.py/p_14-05_bid.py
@@ -1,11 +1,28 @@
 history = {}
-time = {}
+order = 0
 while True:
     bid = input()
     if bid == 'end': break
-    [bid, price] = bid.split(" ")
-    history[bid] = float(price)
-    if bid not in time:
-        time[bid] = 1
-    else: time[bid] += 1
-print(f"{history} {time}")
+    elif bid == '': continue
+    name, price = bid.split(" ")
+    price = float(price)
+    
+    if name not in history:
+        history[name] = {"price": 0, "time": 0, "order": 0}
+        
+    history[name]["price"] = price
+    history[name]["time"] += 1
+    order += 1
+    history[name]["order"] = order
+
+history = dict(sorted(history.items()))
+# kong bid at the price of 300.0 baht in 2 times.
+for k, v in history.items():
+    if v['time'] > 1:
+        print(f"{k} bid at the price of {v['price']} baht in {v['time']} times.")
+    else: print(f"{k} bid at the price of {v['price']} baht in {v['time']} time.")
+    # print(k, v)
+
+if history != {}:
+    winner = max(history, key=lambda k: (history[k]['price'], -history[k]['order']))
+    print(f"The winner is {winner}.")
